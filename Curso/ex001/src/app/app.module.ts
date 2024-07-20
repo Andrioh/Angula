61a2b27c6b76e1ab5f5fd7de46358632c5a9e70c
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-07-20 10:13:12 -0300

    ng for aula 9

--- Curso/ex001/src/app/app.module.ts
+++ Curso/ex001/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './Pages/home/home.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
 import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { UsersComponent } from './Pages/home/users/users.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BaseUiComponent } from './_components/base-ui/base-ui.component';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
-    BaseUiComponent
+    BaseUiComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
