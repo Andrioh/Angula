1234dd352360ff3c5ca4d0bfa30a2d3bb2ec543b
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-03 05:38:53 -0300

    cub-index1

--- TaskHub/FrontEnd/src/app/Pages/Index/index.component.ts
+++ TaskHub/FrontEnd/src/app/Pages/Index/index.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { CubIndex1Component } from "../../components/cub-index1/cub-index1.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, CubIndex1Component],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
---
+++ TaskHub/FrontEnd/src/app/components/cub-index1/cub-index1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cub-index1',
+  standalone: true,
+  imports: [],
+  templateUrl: './cub-index1.component.html',
+  styleUrl: './cub-index1.component.css'
+})
+export class CubIndex1Component {
+
+}
