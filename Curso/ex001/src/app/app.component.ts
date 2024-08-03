d489e8e98ff2b8c6848f766c30738be7f9eded2f
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-03 00:35:08 -0300

    aula 20 persistir dados

--- Curso/ex001/src/app/app.component.ts
+++ Curso/ex001/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './_services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ex001';
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  constructor(private UserService: UserService){
+
+  }
+
+  getUser(){
+    const user = localStorage.getItem('user');
+    this.UserService.setUser( user ? JSON.parse(user): undefined)
+  }
 }
