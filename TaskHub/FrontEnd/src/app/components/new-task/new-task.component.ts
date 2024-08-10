f0dd79eb0788e6f5f8f92736e0f9b636e008ce23
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-10 01:46:06 -0300

    .

--- TaskHub/FrontEnd/src/app/components/new-task/new-task.component.ts
+++ TaskHub/FrontEnd/src/app/components/new-task/new-task.component.ts
@@ -2,18 +2,19 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ShareService } from '../../service/share.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './new-task.component.html',
   styleUrls: ['./new-task.component.css']
 })
 export class NewTaskComponent {
   MostrarTask: boolean = false;
 
-  constructor(private router: Router, private ServiceValue: ShareService){
+  constructor(private router: Router, private ServiceValue: ShareService) {
     this.MostrarTask = this.ServiceValue.ObterTaskValue();
   }
 
@@ -22,4 +23,22 @@ export class NewTaskComponent {
 
     this.ServiceValue.AlterTaskValue(!this.ServiceValue.ObterTaskValue)
   }
+
+  Name: string = '';
+  Category: string = '';
+
+  CreateCub() {
+
+    if (this.Name.trim() === "" || this.Category.trim() === "") {
+      console.log('[ERRO] Veja se inseriu os dados corretamente!');
+    }else{
+      console.log(this.Name);
+      console.log(this.Category);
+    
+      this.Name = "";
+      this.Category = "";
+
+      this.router.navigate([""]);
+    }
+  }    
 }
