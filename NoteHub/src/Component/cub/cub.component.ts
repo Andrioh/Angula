a4c2866f94be29617770a23a484117f00d7ce612
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-16 21:13:47 -0300

    Start of a new project

---
+++ NoteHub/src/Component/cub/cub.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cub',
+  standalone: true,
+  imports: [],
+  templateUrl: './cub.component.html',
+  styleUrl: './cub.component.css'
+})
+export class CubComponent {
+
+}
