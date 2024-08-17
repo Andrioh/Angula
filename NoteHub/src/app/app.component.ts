d5e4a8313b6f14b477339ed7357e7c211868210a
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-16 21:49:59 -0300

    Components created

--- NoteHub/src/app/app.component.ts
+++ NoteHub/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../Component/header/header.component";
+import { CubComponent } from "../Component/cub/cub.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CubComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
